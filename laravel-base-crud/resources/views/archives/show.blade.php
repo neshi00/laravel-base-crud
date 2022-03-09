4035081c1920f57eed39f177d66f946e2f9d4172
neshi00 <luca.ascione85@gmail.com>
2022-03-09 15:57:46 +0100

    fix web show

--- laravel-base-crud/resources/views/archives/show.blade.php
+++ laravel-base-crud/resources/views/archives/show.blade.php
@@ -4,5 +4,10 @@
 
 <h1>Dettagli fumetto #{{ $archive->id }}</h1>
 
+<h1>Titolo: {{ $archive->title }}</h1>
+<h3>Prezzo: {{ $archive->price }}</h3>
+<p>Descrizione: {{ $archive->description }}</p>
+<p>Serie: {{ $archive->series }}</p>
+
 
 @endsection
